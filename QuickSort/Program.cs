2830198262aeda67a1e160ab287cc081be068955
Luke Barkess <luke.barkess@icloud.com>
2022-09-12 14:06:23 +0100

    Add project files.

---
+++ QuickSort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        List<int> QuickSort(List<int> listToSort)
+        {
+            
+        }
+    }
+}
