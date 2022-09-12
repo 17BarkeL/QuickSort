fd43039cbb2b62ca514b75194a39b547a0bc8ad7
Luke Barkess <luke.barkess@icloud.com>
2022-09-12 14:16:48 +0100

    hello

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -10,12 +10,30 @@ namespace QuickSort
     {
         static void Main(string[] args)
         {
+            BubbleSort(new List<int>{1, 5, 2, 8, 6});
 
+            Console.ReadLine();
         }
 
-        List<int> QuickSort(List<int> listToSort)
+        /*List<int> QuickSort(List<int> listToSort)
         {
             
+        }*/
+
+        static void BubbleSort(List<int> listToSort)
+        {
+            int item1 = 0;
+            int item2 = 0;
+
+            for (int pass = 0; pass < listToSort.Count - 1; pass++) // Number of passes
+            {
+                Console.WriteLine("--------");
+
+                for (int comparison = 0; comparison < listToSort.Count - pass; comparison++)
+                {
+                    Console.WriteLine(comparison);
+                }
+            }
         }
     }
 }
