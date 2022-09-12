6b9335ee2f21d4ef9a5c2fc5f850817d7fe560d6
Brumus14 <luke.barkess@icloud.com>
2022-09-12 19:48:45 +0100

    bubbles

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -10,33 +10,69 @@ namespace QuickSort
     {
         static void Main(string[] args)
         {
-            BubbleSort(new List<int>{1, 5, 2, 8, 6});
+            List<int> listToSort = new List<int> { 1, 5, 2, 8, 6 };
+            OutputList(listToSort);
+            OutputList(BubbleSort(listToSort));
 
             Console.ReadLine();
         }
 
-        /*List<int> QuickSort(List<int> listToSort)
+        static List<int> BubbleSort(List<int> list)
         {
-            
-        }*/
-
-        static void BubbleSort(List<int> listToSort)
-        {
+            List<int> sortingList = list;
             int item1 = 0;
             int item2 = 0;
 
-            for (int pass = 0; pass < listToSort.Count; pass++) // Number of passes
+            for (int pass = 0; pass < sortingList.Count; pass++) // Number of passes
             {
-                Console.WriteLine("--------");
-
-                for (int comparison = 0; comparison < listToSort.Count - pass; comparison++)
+                for (int comparison = 0; comparison < sortingList.Count - (pass + 1); comparison++)
                 {
-                    item1 = listToSort[comparison];
-                    item2 = listToSort[(comparison + 1, listToSort.Count)];
+                    item1 = sortingList[comparison];
+                    item2 = sortingList[comparison + 1];
 
-                    Console.WriteLine(item1 + ", " + item2);
+                    if (item1 > item2)
+                    {
+                        sortingList[comparison] = item2;
+                        sortingList[comparison + 1] = item1;
+                    }
+
                 }
             }
+
+            return sortingList;
+        }
+
+        static void QuickSort(List<int> list)
+        {
+            List<int> sortingList = list;
+            List<List<int>> sortingSublists = new List<List<int>>();
+            List<int> pivotIndexes = new List<int>();
+
+
+            int pivotIndex = (int)Math.Ceiling((double)(sortingList.Count - 1) / 2);
+            int pivot = sortingList[pivotIndex];
+
+            foreach (int item in sortingList)
+            {
+                if (item < pivot)
+                {
+                    //sortingSublists
+                }
+            }
+            
+
+        }
+
+        static void OutputList(List<int> list)
+        {
+            string lineToOutput = "";
+
+            foreach (int item in list)
+            {
+                lineToOutput += item == list.Last() ? item.ToString() : item.ToString() + ", ";
+            }
+
+            Console.WriteLine(lineToOutput);
         }
     }
 }
